157ee1cbcd2137e7d479e40bff45d8dc41412b7e
Kyle <56949068+mrzakch@users.noreply.github.com>
2020-10-23 18:41:32 -0400

    Chutes Support Added.

---
+++ src/crewmatic/Chute.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crewmatic;
+
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.MouseInfo;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseEvent;
+
+/**
+ *
+ * @author Kyle
+ */
+public class Chute extends Task {
+
+    @Override
+    public void execute() throws InterruptedException, AWTException {
+        r = new Robot();
+        goTo(1792, 937);
+        r.mousePress(MouseEvent.BUTTON1_DOWN_MASK);
+        Thread.sleep(20);
+        r.mouseRelease(MouseEvent.BUTTON1_DOWN_MASK);
+        Thread.sleep(500);
+        goTo(1320, 420);
+        r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        Thread.sleep(100);
+        for (int i = 420; i < 820; i += 50) {
+            goTo(1320, i);
+            Thread.sleep(50);
+        }
+        Thread.sleep(2250);
+        r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        while (true) {
+            Thread.sleep(100);
+            if (isCompleted()) {
+                break;
+            }
+        }
+    }
+
+    private void goTo(int x, int y) throws AWTException, InterruptedException {
+        for (int count = 0; (MouseInfo.getPointerInfo().getLocation().getX() != x
+                || MouseInfo.getPointerInfo().getLocation().getY() != y)
+                && count < 100; count++) {
+            new Robot().mouseMove(x, y);
+        }
+    }
+
+    @Override
+    public boolean isCompleted() {
+        return (!r.getPixelColor(460, 115).equals(new Color(238, 238, 238)));
+    }
+}
